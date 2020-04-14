8e6ffeb08c82014e267aafccb53f07aeebf6eec8
Hritik Suman <49412100+Hritiksum@users.noreply.github.com>
2020-04-14 17:29:27 +0530

    bst

--- BSt.cpp
+++ BSt.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//tree node
 class node{
 public:
     int data;
@@ -9,6 +10,27 @@ public:
     node(int d):data(d),right(NULL),left(NULL){}
 };
 
+//check whether two tree are identical or not
+bool identical(node* t1,node *t2){
+    if(t1==NULL && t2==NULL){
+        return true;
+    }
+    else if(t1!=NULL && t2==NULL){
+        return false;
+    }
+    else if(t1==NULL && t2!=NULL){
+        return false;
+    }
+    else{
+        if(t1->data==t2->data && identical(t1->left,t2->left) && identical(t1->right,t2->right)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+//count number of leaf node
 int countLeafnode(node* temp){
     if(temp==NULL){
         return 0;
@@ -19,8 +41,23 @@ int countLeafnode(node* temp){
     return countLeafnode(temp->left)+countLeafnode(temp->right);
 }
 
+//create an mirror of tree
+node* mirror(node* root1,node* &root2){
+    if(root1==NULL){
+        return NULL;
+    }
+    if(root2==NULL){
+        node* temp=new node(root1->data);
+        temp->left=root1->right;
+        temp->right=root1->left;
+        return temp;
+    }
+    mirror(root1->left,root2);
+    mirror(root1->right,root2);
+    return root2;
+}
 
-
+//return min value
 node* MinValueInTree(node* temp){
     node* Cache= temp;
     while(Cache && Cache->left!=NULL){
@@ -29,6 +66,7 @@ node* MinValueInTree(node* temp){
     return Cache;
 }
 
+//to delete node
 node* Delete(node* &temp, int data){
     if(temp==NULL){
         return NULL;
@@ -57,6 +95,7 @@ node* Delete(node* &temp, int data){
     return temp;
 }
 
+//to balance tree using vector
 node* BalancedTree(vector <node*> v, int Start,int End){
     if(Start>End){
         return NULL;
@@ -68,6 +107,7 @@ node* BalancedTree(vector <node*> v, int Start,int End){
     return temp;
 }
 
+//to import tree into vector
 void TreeIntoVector(node* temp, vector <node*> &v){
     if(temp==NULL){
         return;
@@ -77,6 +117,7 @@ void TreeIntoVector(node* temp, vector <node*> &v){
     TreeIntoVector(temp->right,v);
 }
 
+//call to balance tree
 node* DoBalance(node* &root){
     vector <node*> v;
     TreeIntoVector(root,v);
@@ -91,6 +132,7 @@ public:
     bool balance;
 };
 
+//to check whether tree is balance or not and also return its height,diameter
 Pair IsBalance(node* temp){
     Pair p;
     if(temp==NULL){
@@ -119,6 +161,7 @@ Pair IsBalance(node* temp){
     return p;
 }
 
+//print tree in inorder
 void Inorder(node* temp){
     if(temp==NULL){
         return;
@@ -128,6 +171,7 @@ void Inorder(node* temp){
     Inorder(temp->right);
 }
 
+//print in lever order
 void LevelOrder(node* temp){
     queue <node*> q;
     q.push(temp);
@@ -154,6 +198,7 @@ void LevelOrder(node* temp){
 
 }
 
+//insert value in tree
 node* InsertValue(node* &temp,int data){
     if(temp==NULL){
         temp=new node(data);
@@ -168,6 +213,7 @@ node* InsertValue(node* &temp,int data){
     return temp;
 }
 
+//to create tree
 void CreateTree(node* &root){
     int data;
     node* temp=NULL;
@@ -180,47 +226,103 @@ void CreateTree(node* &root){
 }
 
 int main(){
-    node* root=NULL;
-    cout<<"Input Data"<<endl;
-    CreateTree(root);
-    cout<<endl<<"Inorder"<<endl;
-    Inorder(root);
-    cout<<endl<<endl<<"LevelOrder"<<endl;
-    LevelOrder(root);
-    Pair p=IsBalance(root);
-    if(p.balance){
-        cout<<endl<<"Tree is balance and it's height is "<<p.height<<" and its diameter is "<<p.diameter;
+
+    node* root1=NULL;
+    node* root2=NULL;
+    cout<<"Input data in first tree 'root1'"<<endl;
+    CreateTree(root1);
+    cout<<"1st tree Inorder print"<<endl;
+    Inorder(root1);
+    cout<<endl;
+    cout<<"1st tree LevelOrder print"<<endl;
+    LevelOrder(root1);
+    Pair p1;
+    p1=IsBalance(root1);
+    if(p1.balance){
+        cout<<"1st tree is balanced ,height= "<<p1.height<<" ,diameter= "<<p1.diameter<<endl;
     }
     else{
-        cout<<endl<<"Tree is not balanced and it's height is "<<p.height<<" and its diameter is "<<p.diameter<<endl;
+        cout<<"1st tree is not balanced ,height= "<<p1.height<<" ,diameter= "<<p1.diameter<<endl;
         cout<<endl<<"BALANCING TREE...."<<endl;
-        root=DoBalance(root);
-        LevelOrder(root);
-        p=IsBalance(root);
-        cout<<endl<<"Tree is balanced now and its height is "<<p.height<<" and its diameter is "<<p.diameter;
+        root1=DoBalance(root1);
+        p1=IsBalance(root1);
+        LevelOrder(root1);
+        cout<<"1st tree is balanced now,height= "<<p1.height<<" ,diameter= "<<p1.diameter<<endl;
     }
-    cout<<endl<<"Want to delete number: if yes-input '1' if no-input '0'";
-    bool v;
-    cin>>v;
-    if(v==true){
-        cout<<endl<<"which number wants to delete"<<endl;
-        int key;
-        cin>>key;cout<<endl;
-        while(v){
-            root=Delete(root,key);
-            LevelOrder(root);
-            cout<<endl<<"Want to delete again: if yes-input '1' if no-input '0'"<<endl;
-            cin>>v;
-            cout<<endl;
-            if(v==false){
-                break;
-            }
-            cout<<endl<<"which number wants to delete"<<endl;
-            cin>>key;
-            cout<<endl;
-        }
-        int leaf=countLeafnode(root);
-        cout<<"Number of leaf node in tree is "<<leaf;
+    int leaf=countLeafnode(root1);
+    cout<<endl<<"Number of leaf node in tree is "<<leaf<<endl;
+
+    root2=mirror(root1,root2);
+    cout<<"mirror of 1st tree and store it in another node 'root2'"<<endl;
+    LevelOrder(root2);
+    cout<<endl<<"to check tree are same or not"<<endl;
+    bool idt=identical(root1,root1);
+    if(idt){
+        cout<<"yes, both tree are true";
     }
+    else{
+        cout<<"No, tree are not identical";
+    }
+
+
+//    cout<<"Input data in second tree 'root1'"<<endl;
+//    CreateTree(root2);
+//    cout<<"2st tree Inorder print"<<endl;;
+//    Inorder(root2);
+//    cout<<"2st tree LevelOrder print"<<endl;
+//    LevelOrder(root2);
+//    Pair p2=IsBalance(root2 );
+//    if(p2.balance){
+//        cout<<"2st tree is balanced ,height= "<<p2.height<<" ,diameter= "<<p2.diameter<<endl;
+//    }
+//    else{
+//        cout<<"2st tree is not balanced ,height= "<<p2.height<<" ,diameter= "<<p2.diameter<<endl;
+//        cout<<endl<<"BALANCING TREE...."<<endl;
+//        root2=IsBalance(root2);
+//        cout<<"2st tree is balanced now,height= "<<p2.height<<" ,diameter= "<<p2.diameter<<endl;
+//    }
+
+
+//    node* root=NULL;
+//    cout<<"Input Data"<<endl;
+//    CreateTree(root);
+//    cout<<endl<<"Inorder"<<endl;
+//    Inorder(root);
+//    cout<<endl<<endl<<"LevelOrder"<<endl;
+//    LevelOrder(root);
+//    Pair p=IsBalance(root);
+//    if(p.balance){
+//        cout<<endl<<"Tree is balance and it's height is "<<p.height<<" and its diameter is "<<p.diameter;
+//    }
+//    else{
+//        cout<<endl<<"Tree is not balanced and it's height is "<<p.height<<" and its diameter is "<<p.diameter<<endl;
+//        cout<<endl<<"BALANCING TREE...."<<endl;
+//        root=DoBalance(root);
+//        LevelOrder(root);
+//        p=IsBalance(root);
+//        cout<<endl<<"Tree is balanced now and its height is "<<p.height<<" and its diameter is "<<p.diameter;
+//    }
+//    cout<<endl<<"Want to delete number: if yes-input '1' if no-input '0'";
+//    bool v;
+//    cin>>v;
+//    if(v==true){
+//        cout<<endl<<"which number wants to delete"<<endl;
+//        int key;
+//        cin>>key;cout<<endl;
+//        while(v){
+//            root=Delete(root,key);
+//            LevelOrder(root);
+//            cout<<endl<<"Want to delete again: if yes-input '1' if no-input '0'"<<endl;
+//            cin>>v;
+//            cout<<endl;
+//            if(v==false){
+//                break;
+//            }
+//            cout<<endl<<"which number wants to delete"<<endl;
+//            cin>>key;
+//            cout<<endl;
+//        }
+
+//    }
     return 0;
 }
